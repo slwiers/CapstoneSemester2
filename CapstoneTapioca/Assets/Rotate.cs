b01b2c7b4f62e521c3702cb8f9a28491cbd38e76
Luis Gustavo Rivera de Leon <luisgriveradeleon1694@gmail.com>
2026-02-01 13:38:29 -0600

    WaterPuzzlealmostdone

--- CapstoneTapioca/Assets/Rotate.cs
+++ CapstoneTapioca/Assets/Rotate.cs
@@ -4,15 +4,75 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
+    float[] rotations = { 0, 90, 180, 270 };
+
+    public float[] correctRotation;
+    [SerializeField]
+    bool isPlaced = false;
+
+    int PossibleRots = 1;
+
+    //ManagePipes gameManager;
+
+    //private void Awake()
+    //{
+    //    gameManager = GameObject.Find("ManagePipes").GetComponent<ManagePipes>();
+    //}
+
     // Start is called before the first frame update
     void Start()
     {
+        PossibleRots = correctRotation.Length;
+        int rand = Random.Range(0, rotations.Length);
+        transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
+
+        if(PossibleRots > 1)
+        {
+            if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1])
+            {
+                isPlaced = true;
+                //gameManager.correctMove();
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.z == correctRotation[0])
+            {
+                isPlaced = true;
+                //gameManager.correctMove();
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        transform.Rotate(new Vector3(0, 0, 90));
+        if (PossibleRots > 1)
+        {
+            if (transform.eulerAngles.z == correctRotation[0] || transform.eulerAngles.z == correctRotation[1] && isPlaced == false)
+            {
+                isPlaced = true;
+                //gameManager.correctMove();
+            }
+            else if (isPlaced == true)
+            {
+                isPlaced = false;
+                //gameManager.wrongMove();
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.z == correctRotation[0] && isPlaced == false)
+            {
+                isPlaced = true;
+                //gameManager.correctMove();
+            }
+            else if (isPlaced == true)
+            {
+                isPlaced = false;
+                //gameManager.wrongMove();
+            }
+        }
     }
 }
