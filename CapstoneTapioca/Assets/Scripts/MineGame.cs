6cbdbc461a4d06218d6798fb2a63d4a5561fec4f
Luis Gustavo Rivera de Leon <luisgriveradeleon1694@gmail.com>
2026-03-31 19:18:24 -0500

    minesweeper finished

--- CapstoneTapioca/Assets/Scripts/MineGame.cs
+++ CapstoneTapioca/Assets/Scripts/MineGame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using UnityEngine;
 
 public class MineGame : MonoBehaviour
@@ -10,6 +11,7 @@ public class MineGame : MonoBehaviour
 
     private Minesweeper minesweeper;
     private Cells[,] state;
+    private bool gameover;
 
     private void Awake ()
     {
@@ -24,10 +26,11 @@ public class MineGame : MonoBehaviour
     private void NewGame()
     {
         state = new Cells[width, height];
+        gameover = false;
         GenerateCells();
         GenerateMines();
         GenerateNumbers();
-        Camera.main.transform.position = new Vector3(width / 2, height / 2, -15);
+        Camera.main.transform.position = new Vector3(width / 2, height / 2, -width);
         minesweeper.Draw(state);
     }
 
@@ -117,15 +120,27 @@ public class MineGame : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(1))
+        if (!gameover)
         {
-            Flag();
+            if (Input.GetMouseButtonDown(1))
+            {
+                Flag();
+                Debug.Log("input.mouseposition" + Input.mousePosition);
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                Revealed();
+                Debug.Log("input.mouseposition" + Input.mousePosition);
+            }
         }
     }
 
     private void Flag()
     {
-        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 worldPos = Input.mousePosition;
+        worldPos.z = 10; // -Camera.main.transform.position.z;
+
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(worldPos);
         Vector3Int cellPosition = minesweeper.tilemap.WorldToCell(worldPosition);
         Cells cell = GetCell(cellPosition.x, cellPosition.y);
         if (cell.type == Cells.Type.Invalid || cell.revealed)
@@ -136,6 +151,109 @@ public class MineGame : MonoBehaviour
         cell.flagged = !cell.flagged;
         state[cellPosition.x, cellPosition.y] = cell;
         minesweeper.Draw(state);
+        Debug.Log("world position" + worldPosition);
+        Debug.Log("cell position" + cellPosition);
+    }
+
+    private void Revealed()
+    {
+        Vector3 worldPos = Input.mousePosition;
+        worldPos.z = 10; // -Camera.main.transform.position.z;
+
+        Vector3 worldPosition = Camera.main.ScreenToWorldPoint(worldPos);
+        Vector3Int cellPosition = minesweeper.tilemap.WorldToCell(worldPosition);
+        Cells cell = GetCell(cellPosition.x, cellPosition.y);
+        if (cell.type == Cells.Type.Invalid || cell.revealed || cell.flagged)
+        {
+            return;
+        }
+
+        switch (cell.type)
+        {
+            case Cells.Type.Mine:
+                Explode(cell); 
+                break;
+            case Cells.Type.Empty:
+                Flood(cell);
+                CheckWinCondition();
+                break;
+            default:
+                cell.revealed = true;
+                state[cellPosition.x, cellPosition.y] = cell;
+                CheckWinCondition();
+                break;
+
+        }
+        minesweeper.Draw(state);
+        Debug.Log ("world position" + worldPosition);
+        Debug.Log("cell position" + cellPosition);
+    }
+
+    private void Flood(Cells cell)
+    {
+        if (cell.revealed) return;
+        if (cell.type == Cells.Type.Mine || cell.type == Cells.Type.Invalid) return;
+
+        cell.revealed = true;
+        state[cell.position.x, cell.position.y] = cell;
+
+        if (cell.type == Cells.Type.Empty)
+        {
+            Flood(GetCell(cell.position.x - 1, cell.position.y));
+            Flood(GetCell(cell.position.x + 1, cell.position.y));
+            Flood(GetCell(cell.position.x, cell.position.y - 1));
+            Flood(GetCell(cell.position.x, cell.position.y + 1));
+        }
+    }
+ 
+    private void Explode(Cells cell)
+    {
+        Debug.Log("Game Over!");
+        gameover = true;
+
+        cell.revealed = true;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                cell = state[x, y];
+                if(cell.type == Cells.Type.Mine)
+                {
+                    cell.revealed = true;
+                    state[x, y] = cell;
+                }
+            }
+        }
+    }
+
+    private void CheckWinCondition()
+    {
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cells cell = state[x, y];
+                if (cell.type != Cells.Type.Mine && !cell.revealed)
+                {
+                    return;
+                }
+            }
+        }
+        Debug.Log("Winner");
+        gameover = true;
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Cells cell = state[x, y];
+                if (cell.type == Cells.Type.Mine)
+                {
+                    cell.flagged = true;
+                    state[x, y] = cell;
+                }
+            }
+        }
     }
 
     private Cells GetCell(int x, int y)
