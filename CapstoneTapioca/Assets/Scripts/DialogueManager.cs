d8fe42aea37d3f09f9362f2513a8a33bfecb7c74
slwiers <slwiers@ilstu.edu>
2026-04-21 20:40:30 -0500

    finished game

--- CapstoneTapioca/Assets/Scripts/DialogueManager.cs
+++ CapstoneTapioca/Assets/Scripts/DialogueManager.cs
@@ -84,6 +84,8 @@ public class DialogueManager : MonoBehaviour
     private bool playedsound1 = true;
     private bool playedsound2 = true;
     private bool playedsound3 = true;
+
+    public GameObject loadingObject;
     private void Awake()
     {
         if (instance != null)
@@ -312,7 +314,7 @@ public class DialogueManager : MonoBehaviour
                     }
                     if (tags[0] == "introtrans")
                     {
-                        loadingScreen.SetActive(true);
+                        loadingObject.SetActive(true);
                         levelLoader.LoadLevel(6);
                         Debug.Log("Scene Loaded");
 
